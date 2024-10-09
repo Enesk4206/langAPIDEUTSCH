e06681a910faf91ef65ad3ec8008bd75aa15cdde
EnesKaya <130606254+Enesk1999@users.noreply.github.com>
2024-10-09 22:42:56 +0300

    update last codes

--- src/lang.js
+++ src/lang.js
@@ -1,4 +1,5 @@
 let germanWords = []; // Kelimeler dizisini başlat
+let arrList = []
 
 // JSON verisini yüklemek için bir fonksiyon
 const langFromJSON = (url) => {
@@ -32,7 +33,25 @@ document.getElementById('userForm').addEventListener("submit", function(event) {
 
     // Kelime bulunduysa, ekranda göster
     if (foundWord) {
-        divResult.innerHTML = `<p class="text-red-500">${foundWord.turkish}</p>`; // Türkçe çeviriyi göster
+        // divResult.innerHTML = `<p class=" p-5 my-4 border border-solid border-cyan-700 text-center bg-black text-white shadow"> ${foundWord.german} ===>  ${foundWord.turkish}</p>`; // Türkçe çeviriyi göster
+
+            arrList.push(foundWord)
+            let output = '';
+            arrList.forEach(word=>{
+                if(arrList.length<=10){
+                    output+= `<p class="w-1/6 p-5 my-4 border border-solid border-cyan-700 text-center bg-black text-white shadow"> ${word.german} ===>  ${word.turkish}</p>`
+                }
+                else{
+                    output =`<p>10 taneden fazla yazılamaz</p>`
+                }
+            })
+        
+            divResult.innerHTML =output;
+ 
+        
+        
+
+        
     } else {
         divResult.innerHTML = `<p class="text-red-500">Kelime bulunamadı. Lütfen tekrar deneyin.</p>`; // Hata mesajı
     }
